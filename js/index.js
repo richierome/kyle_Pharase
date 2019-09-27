d66e3bb29c39250e16cde1b260bee633ee251628
richie <rickromero99@gmail.com>
2019-09-27 15:24:49 -0700

    first push

---
+++ js/index.js
@@ -0,0 +1,10 @@
+
+// require("dotenv").config();
+// var express = require("express");
+// var app = express();
+// var PORT = process.env.PORT || 3000;
+
+// // Middleware
+// app.use(express.urlencoded({ extended: false }));
+// app.use(express.json());
+// app.use(express.static("public"));
