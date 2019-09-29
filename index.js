cece98c31d0422eee18689cb46a1359d6db9e109
richie <rickromero99@gmail.com>
2019-09-28 18:01:27 -0700

    second push

--- index.js
+++ index.js
@@ -2,8 +2,16 @@ const express = require ('express');
 const app = express ();
 const path = require ('path');
 
+// app.use(express.static('css'))
+// app.use(express.static('img'))
+// app.use(express.static('js'))
+// app.use(express.static('vendor'))
+
+app.use('/static', express.static(path.join(__dirname, "public")))
+
+
 app.get('/', function (req, res){
-    res.sendFile(path.join(_dirname, "index.html"));
+    res.sendFile(path.join(__dirname, "index.html"));
 
 });
 
